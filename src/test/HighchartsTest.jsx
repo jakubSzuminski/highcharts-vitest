7f59404e6056940b5b6de62ea47af5b54b31ff95
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2023-03-09 10:36:13 +0100

    Updated the tests.

--- src/test/HighchartsTest.jsx
+++ src/test/HighchartsTest.jsx
@@ -2,11 +2,26 @@ import { useRef, useEffect } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
+
+
 function App(props) {
     const chart = useRef(null);
 
+    const options = {
+        series: [{
+            data: [[Date.UTC(2022, 11, 12), 2.2027296E8], [Date.UTC(2022, 11, 13), 2.10952752E8]]
+        }],
+    
+        tooltip: {
+            formatter() {
+                return this.x == Date.UTC(2022, 11, 12) ? 'FIRST' : 'OTHER'
+            }
+        }
+    }
+
     useEffect(() => {
         if (chart) {
+            // we push our chart object to the Vitest
             props.onCreateChart(chart.current.chart);
         }
     }, [props.onCreateChart]);
@@ -15,7 +30,7 @@ function App(props) {
         <div className="App">
             <HighchartsReact
                 highcharts={Highcharts}
-                options={props.options}
+                options={options}
                 ref={chart}
             />
         </div>
--- src/test/tooltipFormatter.test.jsx
+++ src/test/tooltipFormatter.test.jsx
@@ -2,48 +2,42 @@ import React from 'react';
 import { vi, describe, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import Highcharts from 'highcharts';
 import HighchartsTest from './HighchartsTest';
-
-const options = {
-    series: [{
-        data: [1, 2, 3]
-    }, {
-        data: [3, 3, 2]
-    }]
-}
 
 describe('HC - Tooltip Formatter', () => {
     test('Chart should have a tooltip', () => {
         const onCreateChart = vi.fn();
-        render(<HighchartsTest onCreateChart={onCreateChart} options={{...options}}/>);
-        const chart = onCreateChart.mockReturnValue().calls[0][0]
+
+        // this renders our test chart component
+        render(<HighchartsTest onCreateChart={onCreateChart}/>);
+
+        // here we receive the chart object from the onCreateChart 
+        // Vitest function (which is called in useEffect in the component)
+        const chart = onCreateChart.mockReturnValue().calls[0][0]; 
     
+        // here we check if the tooltip object exists on chart
         expect(chart.tooltip).toBeDefined();
     });
 
-    test('Return false from tooltip.formatter', (assert) => {
+    test('Testing tooltip formatter', (assert) => {
         const onCreateChart = vi.fn();
         
-        render(<HighchartsTest onCreateChart={onCreateChart} options={{
-            ...options,
-            tooltip: {
-                formatter: function() {
-                    return this.y > 1 ? 'Display' : false;
-                }
-            }
-        }}/>);
+        render(<HighchartsTest onCreateChart={onCreateChart}/>);
 
         const chart = onCreateChart.mockReturnValue().calls[0][0],
-            tooltip = chart.tooltip,
-            p1 = chart.series[0].points[0],
-            p2 = chart.series[0].points[1];
+            tooltip = chart.tooltip;
 
-        expect(chart.series.length).toEqual(2);
+        // here we check if there is only one series in the chart (as in
+        // the options in the component)
+        expect(chart.series.length).toEqual(1);
+
+        // here we check if adding series to the chart works
         chart.addSeries({ data: [2, 5, 2] });
-        expect(chart.series.length).toEqual(3);
+        expect(chart.series.length).toEqual(2);
 
-        expect(tooltip.options.formatter.call({ y: 2 })).toEqual('Display');
-        expect(tooltip.options.formatter.call({ y: 1 })).toEqual(false);
+        // here we see what is going to be returned by the tooltip.formatter
+        // for points 0 and 1 in the first series
+        expect(tooltip.options.formatter.call(chart.series[0].points[0])).toEqual('FIRST');
+        expect(tooltip.options.formatter.call(chart.series[1].points[1])).toEqual('OTHER');
     });
 });
